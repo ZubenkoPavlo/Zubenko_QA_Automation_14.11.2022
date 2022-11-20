d3c2d804914a7904a278931d7d9e7b645ccffadd
Пользователь <zubenko.p@gmail.com>
2022-11-20 19:23:22 +0200

    Added additional work with Git

--- src/main/java/lesson_1/Main.java
+++ src/main/java/lesson_1/Main.java
@@ -3,5 +3,7 @@ package lesson_1;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        System.out.println("My work with Git");
             }
 }
