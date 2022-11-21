260bb87c8c6f658c33874a189c0a0a4f375d3746
ZubenkoPavlo <zubenko.p@gmail.com>
2022-11-21 13:47:12 +0200

    Created new tests in branch for the project

---
+++ src/main/java/lesson_2/NewTestProjekt.java
@@ -0,0 +1,16 @@
+package lesson_2;
+
+public class NewTestProjekt {
+
+    public void test1(){
+        System.out.println("First test");
+    }
+
+    public void test2(){
+        System.out.println("Second test");
+    }
+
+    public void test3(){
+        System.out.println("Third test");
+    }
+}
