a1b51ac4caea5dbb94b59de87f94e9d2e6562c8d
ZubenkoPavlo <zubenko.p@gmail.com>
2022-11-21 14:21:00 +0200

    Created new tests in branch for the project

--- src/main/java/lesson_1/Main.java
+++ src/main/java/lesson_1/Main.java
@@ -5,5 +5,7 @@ public class Main {
         System.out.println("Hello World!");
 
         System.out.println("My work with Git");
+
+        System.out.println("Additional task");
             }
 }
